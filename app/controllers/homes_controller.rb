17ff76f2bf3a6b983fb127115001b3120774ad87
poko-1951 <103019339+poko-1951@users.noreply.github.com>
2022-06-28 22:13:01 +0900

    [Add]wikipedia取得（失敗）

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,13 @@
 class HomesController < ApplicationController
   def top
   end
+
+  def wiki
+    @page = Wikipedia.find("伊勢崎市")
+    @title = @page.title
+    @con = @page.content
+    @content = WikiCloth::Parser.new(data: @page.content) #page.contentはさっき取得した内容
+
+    @content.to_html
+  end
 end
